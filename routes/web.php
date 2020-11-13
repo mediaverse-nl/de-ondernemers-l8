f64d404805db5cb64f3d73057631ffcb0fd18bf0
mediaverse-nl <Overrated13@>
2020-11-13 17:06:37 +0100

    web test

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('locale/{locale}','App\Http\Controllers\LocalizationController')->name('locale');
 
 Route::middleware(['locale'])->group(function () {
-    Route::get('test', function (){
-        abort(403, 'Unauthorized action.');
-    });
+//    Route::get('test', function (){
+//        abort(403, 'Unauthorized action.');
+//    });
 
 
 
